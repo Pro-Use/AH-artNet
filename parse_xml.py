4ffc3e65a7650c95fbcee2e1faf3bf98d5ab9d6f
pro-use <robertprouse@gmail.com>
2019-11-23 17:41:00 +0000

    correct xml parsing

--- parse_xml.py
+++ parse_xml.py
@@ -7,12 +7,14 @@ root = tree.getroot()
 keyframes = root[0][0][0][4]
 start = 0.0
 
-for keyframe in keyframes:
+for i in range(len(keyframes) - 1):
+    keyframe = keyframes[i]
+    next_keyframe = keyframes[i + 1]
     seconds = float(keyframe[0].text) / 30
-    fade_time = seconds - start
+    seconds_to = float(next_keyframe[0].text) / 30
+    fade_time = seconds_to - seconds
     timecode = datetime.timedelta(seconds=seconds)
     h, m, s = str(timecode).split(":")
     timecode = "%s:%0.2f" % (m, float(s))
     print("['%s', light_ctl.fade, {'universe': 'master', 'fade_to': %s, 'fade_time': %s}]," %
-          (timecode, keyframe[1].text, fade_time))
-    start = seconds
+          (timecode, next_keyframe[1].text, fade_time))
